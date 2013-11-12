52977a4ae79d250de8edb08dc0999202340de333
Justice Amoh <justiceamoh@gmail.com>
2013-11-12 17:03:58 -0500

    cleaned up temp.c and fixed minor bugs

--- arduino/temp.c
+++ arduino/temp.c
@@ -1,5 +1,24 @@
 #include "Wire.h"
 #define button 2;
+
+#define START_FREQ_R1 0x82
+#define START_FREQ_R2 0x83
+#define START_FREQ_R3 0x84
+
+#define FREG_INCRE_R1 0x85
+#define FREG_INCRE_R2 0x86
+#define FREG_INCRE_R3 0x87
+
+#define NUM_INCRE_R1 0x88
+#define NUM_INCRE_R2 0x89
+
+#define TEMP_R1 0x92
+#define TEMP_R2 0x93
+
+#define CTRL_REG 0x80
+#define STATUS_REG 0x8F
+
+
 void setup() {
 	Wire.begin();
 	Serial.begin(9600);
@@ -18,77 +37,77 @@ onButtonPressed();
 
 void onButtonPressed(){
 int flag;
-
+boolean tempFlag;
 flag = digitalRead(button);
 
 if(flag)
-	runSweep();
-
+	//runSweep();
+	tempFlag = measureTemperature();
+	Serial.println(tempFlag);
 }
 
 
 void programReg(){
 
-// set PGA gain 1, range 2
-writeData(0x80,0x07,0);
+	// Set Range 2, PGA gain 1
+	writeData(CTRL_REG,0x07,0);
 
-//set settling cycles
-writeData(0x8a, 0x07, 0);
-writeData(0x8b, 0xff, 0);
-
-
-//start frequency of 1kHz
-writeData(0x82, 0, 0);
-writeData(0x83, 0x83, 0);
-writeData(0x84, 0x12, 0);
-
-//increment by 1 kHz
-writeData(0x85, 0, 0); 
-writeData(0x86, 0x83, 0); 
-writeData(0x87, 0x12, 0);
+	// Set settling cycles
+	writeData(0x8a, 0x07, 0);
+	writeData(0x8b, 0xff, 0);
 
 
-//points in frequency sweep (100)
-writeData(0x88, 0, 0);
-writeData(0x89, 0x64, 0);
+	// Start frequency of 1kHz
+	writeData(START_FREQ_R1, 0, 0);
+	writeData(START_FREQ_R2, 0x83, 0);
+	writeData(START_FREQ_R3, 0x12, 0);
 
-//standby '10110000'
-writeData(0x80, 0xb0, 1);
-
-//initialize sweep
-writeData(0x80, 0x10, 1);
-
-
+	// Increment by 1 kHz
+	writeData(FREG_INCRE_R1, 0, 0); 
+	writeData(FREG_INCRE_R2, 0x83, 0); 
+	writeData(FREG_INCRE_R3, 0x12, 0);
 
 
+	// Points in frequency sweep (100), max 511
+	writeData(NUM_INCRE_R1, 0, 0);
+	writeData(NUM_INCRE_R2, 0x64, 0);
+
+	// Standby '10110000'
+	writeData(CTRL_REG, 0xb0, 1);
+
+	// Initialize sweep
+	writeData(CTRL_REG, 0x10, 1);
 
 }
 
 
 void runSweep() {
-int re[100];
-int img[100];
-int i=0;
+	int re[100];
+	int img[100];
+	int i=0;
 
-//start sweep
-writeData(0x80, 0x20, 1);	
+	// Start sweep
+	writeData(CTRL_REG, 0x20, 1);	
 
+		while (true) {
+			
+			// Check for availability of data
+			if ((readData(0x96) & 0x04)==4){
+				writeData(CTRL_REG,0xa0,0);
+				break;
+			}		
 
+			// Real Data
+			re[i]=readData(0x94) + readData(0x95);
+			img[i++]=readData(0x96) + readData(0x97);
 
-	while (true) {
-		
-		//check for availability of data
-		if ((readData(0x96) & 0x04)==4){
-			writeData(0x80,0xa0,0);
-			break;
-		}		
+			Serial.print("Real:");
+			Serial.println(re[i-1]);
 
-		// Real Data
-		re[i]=readData(0x94) + readData(0x95);
-		img[i++]=readData(0x96) + readData(0x97);
-
-
-	}
+			Serial.print("Img:");
+			Serial.println(img[i-1]);
+			Serial.print("\n");	
+		}
 
 
 }
@@ -101,15 +120,55 @@ writeData(0x80, 0x20, 1);
 unsigned int readData(unsigned int addr){
 	Wire.requestFrom(addr, 1);
 	unsigned int data = Wire.read();
-	return data;
+	Wire.endTransmission();
+	return data;	
 }
 
 
-void writeData(byte data, unsigned int addr, unsigned int flag) {
+void writeData(unsigned int addr, unsigned int data,  unsigned int flag) {
 
  Wire.beginTransmission(addr);
- Wire.write(addr); // 
+ Wire.write(data); 
  Wire.endTransmission();
 
 }
 
+
+boolean measureTemperature() {
+  
+	// Measure temperature '10010000'
+	writeData(CTRL_REG, 0x90, 1);
+
+	delay(10); // wait for 10 ms
+
+	int flag = readData(STATUS_REG)& 7;
+
+  if (flag == 1) {
+
+    // Temperature is available
+    int temperatureData = readData(TEMP_R1) << 8;
+    temperatureData |= readData(TEMP_R2);
+    temperatureData &= 0x3FFF; // remove first two bits
+    
+    if (temperatureData & 0x2000 == 1) { // negative temperature
+      
+      temperatureData -= 0x4000;
+    }
+    
+    temperatureData /= 32;
+    
+    Serial.print("Current temperature is ");
+    Serial.print(temperatureData); 
+    Serial.println(" degrees Celsius.");
+    
+
+    // Power Down '10100000'
+    writeData(CTRL_REG,0xa0,1);
+    setControlRegister(POWER_DOWN);
+    
+    return true;
+
+  } else {
+    return false;
+  }
+}
