58a9f98edf935e7a1c5168468e19bcad62c97773
Justice Amoh <justiceamoh@gmail.com>
2013-12-02 12:55:05 -0500

    modified sketch for easy input of frequency sweep parameters

--- arduino/ic_control/src/sketch.ino
+++ arduino/ic_control/src/sketch.ino
@@ -30,7 +30,10 @@
 #define STATUS_REG 0x8F
 
 
-
+const float MCLK = 16.776*pow(10,6); // AD5933 Internal Clock Speed 16.776 MHz
+const float start_freq = 30*pow(10,3); // Set start freq, < 100Khz
+const float incre_freq = 1*pow(10,3); // Set freq increment
+const int incre_num = 10; // Set number of increments; < 511
 
 void setup() {
 	Wire.begin();
@@ -59,18 +62,18 @@ void programReg(){
 	writeData(NUM_SCYCLES_R2, 0xFF);
 
 	// Start frequency of 1kHz
-	writeData(START_FREQ_R1, 0);
-	writeData(START_FREQ_R2, 0x83);
-	writeData(START_FREQ_R3, 0x12);
+	writeData(START_FREQ_R1, getFrequency(start_freq,1));
+	writeData(START_FREQ_R2, getFrequency(start_freq,2));
+	writeData(START_FREQ_R3, getFrequency(start_freq,3));
 
 	// Increment by 1 kHz
-	writeData(FREG_INCRE_R1, 0); 
-	writeData(FREG_INCRE_R2, 0x83); 
-	writeData(FREG_INCRE_R3, 0x12);
+	writeData(FREG_INCRE_R1, getFrequency(incre_freq,1)); 
+	writeData(FREG_INCRE_R2, getFrequency(incre_freq,2)); 
+	writeData(FREG_INCRE_R3, getFrequency(incre_freq,3));
 
 	// Points in frequency sweep (100), max 511
-	writeData(NUM_INCRE_R1, 0);
-	writeData(NUM_INCRE_R2, 0x64);
+	writeData(NUM_INCRE_R1, (incre_num & 0x001F00)>>0x08 );
+	writeData(NUM_INCRE_R2, (incre_num & 0x0000FF));
 
 	// Standby '10110000'
 	writeData(CTRL_REG, 0xB0);
@@ -84,7 +87,9 @@ void programReg(){
 void runSweep() {
 	int re;
 	int img;
+	double freq;
 	double mag;
+	double phase;
 	int i=0;
 
 	// Start sweep
@@ -112,28 +117,34 @@ void runSweep() {
 			img -= 0x10000;
 		}
 
-
+		freq = start_freq + i*incre_freq;
 		mag = sqrt(pow(double(re),2)+pow(double(img),2));
-		//gain, impedance, phase
+		phase = atan(double(img)/double(re));
 
-		Serial.print("Real:");
-		Serial.println(re);
+   		// gain = 2.3 * pow(10, -10);    
+    	// impedance = 1/(gain*magnitude);
 
-		Serial.print("Imag:");
-		Serial.println(img);
+		Serial.print("Freq:");
+		Serial.print(freq/1000);
+		Serial.println("kHz");
+
+		Serial.print("Mag:");
+		Serial.println(mag);
+
+		Serial.print("Phase:");
+		Serial.println(phase);
+		Serial.println(" ");
 		Serial.println(" ");
 		
 		//Increment frequency
 		if((readData(STATUS_REG) & 7) < 4 )
 			writeData(CTRL_REG,0x30);
-
+		i++;
 	}
 
 
 	//Power down
 	writeData(CTRL_REG,0xA0);
-
-
 }
 
 
@@ -145,7 +156,6 @@ void writeData(int addr, int data) {
  Wire.write(data);
  Wire.endTransmission();
  delay(1);
-
 }
 
 
@@ -218,4 +228,29 @@ boolean measureTemperature() {
   } else {
     return false;
   }
+}
+
+
+byte getFrequency(float freq, int n){
+	long val = long((freq/(MCLK/4)) * pow(2,27));
+	byte code;
+
+	  switch (n) {
+	    case 1:
+	      code = (val & 0xFF0000) >> 0x10; 
+	      break;
+	    
+	    case 2:
+	      code = (val & 0x00FF00) >> 0x08;
+	      break;
+
+	    case 3:
+	      code = (val & 0x0000FF);
+	      break;
+
+	    default: 
+	      code = 0;
+	  }
+
+	return code;  
 }
--- arduino/temp/temp.ino
+++ arduino/temp/temp.ino
@@ -201,3 +201,28 @@ boolean measureTemperature() {
     return false;
   }
 }
+
+
+byte getFrequency(float freq, int n){
+	long val = long((freq/(MCLK/4)) * pow(2,27));
+	byte code;
+
+	  switch (n) {
+	    case 1:
+	      code = (val & 0xFF0000) >> 0x10; 
+	      break;
+	    
+	    case 2:
+	      code = (value & 0x00FF00) >> 0x08;
+	      break;
+
+	    case 3:
+	      code = (value & 0x0000FF);
+	      break;
+
+	    default: 
+	      code = 0;
+	  }
+
+	return code;  
+}
